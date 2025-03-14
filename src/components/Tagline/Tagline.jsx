a874985862c196d698f058a02bf01a58977095ea
Dann035 <landadlh603@gmail.com>
2025-03-14 21:29:11 +0100

    first steps in Tagline.jsx and Tagline.css

--- src/components/Tagline/Tagline.jsx
+++ src/components/Tagline/Tagline.jsx
@@ -1,10 +1,23 @@
 import './Tagline.css';
 
+import PruebalosAhora from '../../assets/img/2_pakecomp_pollos_MV.webp'
+import Pakecomartas from '../../assets/img/1_pakecomp_pollos_PC.webp'
+import Stylecroissant from '../../assets/img/999crois_rec_coreg_nopedido_PC.webp'
+
 const Tagline = () => {
   return (
-    <>
-      <h1>Soy el Tagline</h1>
-    </>
+    <div className='tagline-box container-xl p-0'>
+      <div className='tagline-imagen-precio row'>
+        <a href="https://www.just-eat.es/restaurants-croissant-showtime-madrid/menu" target="_blank" rel="noreferrer">
+          <img className='tagline-imagen-croissant' src={Stylecroissant} alt=""/>
+        </a>
+        <span className='tagline-precio-oferta col-12'>9'99€</span>
+      </div>
+      <div className='row'>
+        <img className='col-6 col-lg-6 p-0' src={Pakecomartas} alt="" />
+        <img className='col-6 col-lg-6 p-0' src={PruebalosAhora} alt="" />
+      </div>
+    </div>
   )
 }
 export default Tagline;
