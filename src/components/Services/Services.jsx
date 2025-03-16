67875fe024f7d5676599f1e8c836c99430efb6c2
Blessing-09 <blessingogbogu@gmail.com>
2025-03-16 17:45:19 +0100

    Correcting pages

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,5 +1,7 @@
 import './Services.css';
-import tierMenu from '../../assets/img/02_Tier-Menu-CLAZZICAS-2022.webp';
+import tierMenu1 from '../../assets/img/02_Tier-Menu-DELUXES-2022.png';
+import tierMenu2 from '../../assets/img/02_Tier-Menu-CLAZZICAS-2022.webp';
+import tierMenu3 from '../../assets/img/Crea-tu-pizza-PC.png';
 import texasBbq from '../../assets/img/TexasBBQCrispyDelivery_TS220921.webp';
 
 const Services = () => {
@@ -8,34 +10,33 @@ const Services = () => {
     {/* MAIN CONTAINER STARTS here */}
       <div className='container-fluid d-flex flex-column justify-content-center align-items-center'> 
         <div className='row'>
-          <div className='col-sm-12'>
-             <img src={tierMenu} className="d-block w-100 tierMenu" alt="Image 1"/>
+          <div className='col-sm-12 mb-3'>
+             <img src={tierMenu1} className="d-block w-100 tierMenu" alt="Image 1"/>
           </div>
         </div>
-          {/* First carousel starts here with a div to store it all */}
-        <div className='hello row'>
-          <div className='col-sm-12'>
+        {/* <div className='row'>
+          <div className='col-md-3'> */}
           {/* carouselExample that contains the carousel and buttons starts here */}
-         <div id="carouselExampleControls" className="carousel slide w-100" data-bs-ride="carousel">  
+         <div id="carouselExampleControls" className="carousel slide">  
           {/* carouselInner starts here */}
-          <div className="carousel-inner">
+          <div className="carousel-inner innerCarou">
              {/* First carouselItem starts here */}
-            <div className="carousel-item active">
-              <div className='row'> 
+            <div className="carousel-item active carouItem1">
+              <div className='row d-flex justify-content-around align-items-center'> 
                 <div className='col-sm-3'>
-                  <img src={texasBbq} className="d-block w-100 img-fluid" alt="Image 1"/>
+                  <img src={texasBbq} className="d-block w-100" alt="Image 1"/>
                   <p>Parmesana Carbonara</p>
                 </div>
                 <div className='col-sm-3'>
-                <img src={texasBbq} className="d-block w-100 img-fluid" alt="Image 1"/>
+                <img src={texasBbq} className="d-block w-100" alt="Image 1"/>
                 <p>Cabramelizada</p>
                 </div>
                 <div className='col-sm-3'>
-                <img src={texasBbq} className="d-block w-100 img-fluid" alt="Image 1"/>
+                <img src={texasBbq} className="d-block w-100" alt="Image 1"/>
                 <p>Cremozza Al Estilo Bourbon</p>
                 </div>
                 <div className='col-sm-3'>
-                <img src={texasBbq} className="d-block w-100 img-fluid" alt="Image 1"/>
+                <img src={texasBbq} className="d-block w-100" alt="Image 1"/>
                 <p>Cheesix</p>
                 </div>
             </div>
@@ -43,22 +44,22 @@ const Services = () => {
             {/* First carouselItem ends here */}
             
             {/* Second carouselItem starts here */}
-            <div className="carousel-item">
-              <div className='row'> 
+            <div className="carousel-item carouItem2">
+              <div className='row d-flex justify-content-around align-items-center'> 
                 <div className='col-sm-3'>
-                <img src={texasBbq} className="d-block w-100 img-fluid" alt="Image 1"/>
+                <img src={texasBbq} className="d-block w-100" alt="Image 1"/>
                 <p>Alabama Pulled Pork</p>
                 </div>
                 <div className='col-sm-3'>
-                <img src={texasBbq} className="d-block w-100 img-fluid" alt="Image 1"/>
+                <img src={texasBbq} className="d-block w-100" alt="Image 1"/>
                 <p>Buffalo Chicken</p>
                 </div>
                 <div className='col-sm-3'>
-                <img src={texasBbq} className="d-block w-100 img-fluid" alt="Image 1"/>
+                <img src={texasBbq} className="d-block w-100" alt="Image 1"/>
                 <p>Hawaiana Crispy</p>
                 </div>
                 <div className='col-sm-3'>
-                <img src={texasBbq} className="d-block w-100 img-fluid" alt="Image 1"/>
+                <img src={texasBbq} className="d-block w-100" alt="Image 1"/>
                 <p>Texas BBQ Crispy</p>
                 </div>
               </div>
@@ -66,23 +67,26 @@ const Services = () => {
             {/* Second carouselItem starts here */}
           </div>
           {/* carouselInner ends here */}
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <div>
+          <button className="carousel-control-prev carouselBtn" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon button-prev" aria-hidden="true"></span>
             <span className="visually-hidden">Previous</span>
           </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <button className="carousel-control-next carouselBtn" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span className="carousel-control-next-icon button-next" aria-hidden="true"></span>
             <span className="visually-hidden">Next</span>
           </button>  
+          </div>
         </div>
          {/* carouselExample that contains the carousel and buttons ends here */}
-         </div>
-        </div>
-        {/* And the first carousel MAIN container ends here */}
+         {/* </div> */}
+         {/* carouselContainer ends here */}
+         {/* </div> */}
+        
 
-        <div className='row tierMenu2'>
+        <div className='row tierMenu2 mt-3'>
           <div className='col-sm-12 '>
-             <img src={tierMenu} className="d-block w-100 " alt="Image 1"/>
+             <img src={tierMenu2} className="d-block w-100 " alt="Image 1"/>
           </div>
         </div>
 
